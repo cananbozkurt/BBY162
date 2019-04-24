53a57071c80408585ab61d396395889003c2c53e
cananbozkurt <49165486+cananbozkurt@users.noreply.github.com>
2019-04-24 14:15:37 +0300

    Add files via upload

---
+++ uygulama04.py
@@ -0,0 +1,30 @@
+#1.soru
+
+metin = "Açık bilim, araştırma çıktılarına ve süreçlerine herkesin serbestçe erişmesini, bunların ortak kullanımını, dağıtımını ve üretimini kolaylaştıran bilim uygulamasıdır."
+metin = list(metin)
+for i in range(20):
+    print(metin[i], end="")
+
+#2.soru
+liste = ["Açık Bilim", "Açık Erişim", "Açık Lisans", "Açık Eğitim", "Açık Veri", "Açık Kültür"]
+
+for i in liste:
+    print(i)
+
+#3.soru
+
+sozluk = {“Elma” : “Ağaçta yetişen bir tür meyve” , “Salatalık” : “Fidan üzerinde büyüyen bir tür sebze”}
+
+Bool = false
+While(Bool == true):
+my_input = input("please enter the argument")
+For
+i in list:
+If(i == my_input):
+
+Print(i)
+Print(sözlük[i])
+bool = true
+
+
+
